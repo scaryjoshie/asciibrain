220adf743ba6368984c5d6e0faeca8ea22341afb
olegpolin <olegpolin2@gmail.com>
2024-04-23 23:45:26 -0400

    Add parts image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,14 +9,14 @@
 
     <Header />
 
-    <div class="flex flex-row justify-center gap-8 w-full max-w-[108rem] px-16">
-        <div class="basis-1/6">
+    <div class="flex flex-col md:flex-row max-md:items-center justify-center gap-8 w-full max-w-[108rem] px-4 md:px-16">
+        <div class="order-2 md:order-1 md:basis-1/6">
             <Sidebar />
         </div>
-        <div class="basis-2/3">
+        <div class="order-1 md:order-2 md:basis-2/3">
             <slot />
         </div>
-        <div class="basis-1/6"></div>
+        <div class="max-md:hidden md:order-3 basis-1/6"></div>
     </div>
 
 </div>
