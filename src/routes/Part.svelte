d5e571f2a5aa072da90c41c878885972c187e2e9
olegpolin <olegpolin2@gmail.com>
2024-04-23 21:03:10 -0400

    Add arrow icon

--- src/routes/Part.svelte
+++ src/routes/Part.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { Button } from "$lib/components/ui/button";
+    import Arrow from "lucide-svelte/icons/corner-down-left"
 
     export let name: string;
     export let definition: string;
@@ -11,8 +12,9 @@
 
     <p>{definition}</p>
 
-    <Button variant="ghost" href="/">
+    <Button variant="outline" href="/">
         go back
+        <Arrow class="ml-4 h-4 w-4" />
     </Button>
 
 </div>
