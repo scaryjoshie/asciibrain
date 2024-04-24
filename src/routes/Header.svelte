939cf38f731c37f91eddd9c6975df9f1a11304c5
olegpolin <olegpolin2@gmail.com>
2024-04-24 00:01:50 -0400

    Update sidebar

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,4 +1,4 @@
-<h1 class="text-4xl text-center px-4 py-16 border-b w-full">
+<h1 class="text-4xl text-center px-4 py-8 border-b w-full">
     <a href="/">
         ascii brain
     </a>
--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -2,83 +2,86 @@
     import { Separator } from "$lib/components/ui/separator";
 </script>
 
-<div class="flex flex-col gap-8 border w-fit h-fit p-8 rounded-lg">
+<div class="flex flex-col gap-2 border w-fit h-fit p-8 rounded-lg">
     <h2>brain parts:</h2>
-    <Separator class="-my-4" />
-    <ul class="list-disc flex flex-col gap-4">
-        <li class="underline hover:no-underline">
-            <a href="/cerebral-cortex">
+    <ul class="list-disc flex flex-col gap-2">
+        <li>
+            <a class="underline hover:no-underline" href="/cerebral-cortex">
                 cerebral cortex
             </a>
         </li>
-        <li class="underline hover:no-underline">
-            <a href="/cerebellum">
+        <li>
+            <a class="underline hover:no-underline" href="/cerebellum">
                 cerebellum
             </a>
         </li>
-        <li class="underline hover:no-underline">
-            <a href="/corpus-callosum">
+        <li>
+            <a class="underline hover:no-underline" href="/corpus-callosum">
                 corpus callosum
             </a>
         </li>
-        <li class="underline hover:no-underline">
-            <a href="/thalamus">
+        <li>
+            <a class="underline hover:no-underline" href="/thalamus">
                 thalamus
             </a>
         </li>
-        <li class="underline hover:no-underline">
-            <a href="/hypothalamus">
+        <li>
+            <a class="underline hover:no-underline" href="/hypothalamus">
                 hypothalamus
             </a>
         </li>
-        <li class="underline hover:no-underline">
-            <a href="/hippocampus">
+        <li>
+            <a class="underline hover:no-underline" href="/hippocampus">
                 hippocampus
             </a>
         </li>
-        <li class="underline hover:no-underline">
-            <a href="/amygdala">
+        <li>
+            <a class="underline hover:no-underline" href="/amygdala">
                 amygdala
             </a>
         </li>
-        <li class="underline hover:no-underline">
-            <a href="/pons">
+        <li>
+            <a class="underline hover:no-underline" href="/pons">
                 pons
             </a>
         </li>
-        <li class="underline hover:no-underline">
-            <a href="/medulla">
+        <li>
+            <a class="underline hover:no-underline" href="/medulla">
                 medulla
             </a>
         </li>
-        <li class="underline hover:no-underline">
-            <a href="/brain-stem">
-                brain stem
-            </a>
-        </li>
-        <li class="underline hover:no-underline">
-            <a href="/reticular-formation">
+        <li>
+            <a class="underline hover:no-underline" href="/reticular-formation">
                 reticular formation
             </a>
         </li>
-        <li class="underline hover:no-underline">
-            <a href="/frontal-lobe">
+    </ul>
+    <Separator class="my-4" />
+    <h2>lobes:</h2>
+    <ul class="list-disc flex flex-col gap-2">
+        <li>
+            <a class="underline hover:no-underline" href="/frontal-lobe">
                 frontal lobe
             </a>
         </li>
-        <li class="underline hover:no-underline">
-            <a href="/parietal-lobe">
+        <li>
+            <a class="underline hover:no-underline" href="/parietal-lobe">
                 parietal lobe
             </a>
         </li>
-        <li class="underline hover:no-underline">
-            <a href="/temporal-lobe">
+        <li>
+            <a class="underline hover:no-underline" href="/temporal-lobe">
                 temporal lobe
             </a>
         </li>
-        <li class="underline hover:no-underline">
-            <a href="/occipital-lobe">
+        <li>
+            <a class="underline hover:no-underline" href="/occipital-lobe">
                 occipital lobe
+            </a>
+        </li>
+        <li>
+            <a class="underline hover:no-underline" href="/brain-stem">
+                brain stem
             </a>
         </li>
     </ul>
